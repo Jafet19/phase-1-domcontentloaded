52bf4a9308776642a3caeea78d667c5f206e1384
Jafet19 <kacur99@gmail.com>
2022-09-01 09:07:53 -0500

    commit tree 729fe79487cbad0c03bfcfd8971b8fec6c25de8d

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Your code goes here
+const page =    document.querySelector('p')
+let change = 'This is really cool!'
+document.addEventListener("DOMContentLoaded", () => { console.log("This is really cool!"); page.textContent = 'This is really cool!'})
